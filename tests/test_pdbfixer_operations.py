cc1c19760c001b64512d544c6bb5e4b7f5a601e3
mpds <matheusmullerps@gmail.com>
2024-11-25 07:52:26 -0300

    Add tests to ensure non-standard residues and missing heavy atoms do not raise errors when structs are OK

--- tests/test_pdbfixer_operations.py
+++ tests/test_pdbfixer_operations.py
@@ -4,7 +4,7 @@ from pdbfixer import PDBFixer
 from docktprep.pdbfixer_operations import *
 
 
-def test_no_std_residues_replace():
+def test_no_std_residues_replaces():
     fixer = PDBFixer("tests/data/1bkx.pdb")
     fixer.findNonstandardResidues()
     assert fixer.nonstandardResidues
@@ -13,6 +13,13 @@ def test_no_std_residues_replace():
     fixer = replace.fix(fixer)
     fixer.findNonstandardResidues()
     assert not fixer.nonstandardResidues
+
+
+def test_no_std_residues_does_not_raise():
+    fixer = PDBFixer("tests/data/1az5.pdb")
+    replace = ReplaceNonStdResidues()
+    fixer = replace.fix(fixer)
+    assert fixer
 
 
 def test_adds_missing_heavy_atoms():
@@ -29,6 +36,13 @@ def test_adds_missing_heavy_atoms():
     assert not fixer.missingTerminals
 
 
+def test_missing_heavy_atoms_does_not_raise():
+    fixer = PDBFixer("tests/data/9ins.pdb")
+    add_missing_heavy = AddMissingHeavyAtoms()
+    fixer = add_missing_heavy.fix(fixer)
+    assert fixer
+
+
 def test_adds_missing_residues():
     fixer = PDBFixer("tests/data/1az5.pdb")
     fixer.missingResidues = {}
@@ -39,6 +53,13 @@ def test_adds_missing_residues():
     fixer = add_missing.fix(fixer)
     fixer.findMissingResidues()
     assert not fixer.missingResidues
+
+
+def test_missing_residues_does_not_raise():
+    fixer = PDBFixer("tests/data/9ins.pdb")
+    add_missing_residues = AddMissingResidues()
+    fixer = add_missing_residues.fix(fixer)
+    assert fixer
 
 
 def test_add_missing_hydrogens_does_not_raise():
